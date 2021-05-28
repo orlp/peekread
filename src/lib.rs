57e691ebb137bcdb400798708a28c442a6eb97d3
Orson Peters <orsonpeters@gmail.com>
2021-05-28 20:00:10 +0200

    Compiling implementation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-use std::io::{Error, ErrorKind, BufRead, Result};
+use std::io::{BufRead, Error, ErrorKind, Read, Result, Take};
 
+mod peekreader;
 
 /// A BufRead object that supports peeking. It has a separate 'peek cursor' which
 /// can go ahead of the regular read cursor, but never behind it. In case the
@@ -8,8 +9,12 @@ pub trait PeekRead: BufRead {
     /// Gets the position of the peek cursor relative to the read cursor.
     fn peek_position(&self) -> usize;
 
-    /// Sets the position of the peek cursor.
-    fn set_peek_position(&mut self, pos: usize);
+    /// Sets the position of the peek cursor. Returns the new `peek_position()`.
+    ///
+    /// It's perfectly allowed to set a seek position beyond EOF, it will just
+    /// result in failing reads later. This function can only fail when using
+    /// [`PeekSeekFrom::End`] and an IO error occurred before reaching EOF.
+    fn peek_seek(&mut self, pos: PeekSeekFrom) -> Result<usize>;
 
     /// Pushes the given data into the stream at the front, pushing the read
     /// cursor back. The peek cursor can do three things depending on `peek_cursor_behavior`:
@@ -32,14 +37,11 @@ pub trait PeekRead: BufRead {
     }
 
     fn peek_to_end(&mut self, buf: &mut Vec<u8>) -> Result<usize> {
-
-        // FIXME: implement.
-        Ok(0)
+        default_peek_to_end(self, buf)
     }
 
     fn peek_to_string(&mut self, buf: &mut String) -> Result<usize> {
-        // FIXME: implement.
-        Ok(0)
+        default_peek_to_string(self, buf)
     }
 
     fn by_ref(&mut self) -> &mut Self
@@ -57,28 +59,49 @@ pub trait PeekRead: BufRead {
     }
 }
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum PeekSeekFrom {
+    /// Sets the peek cursor offset from the read cursor by the specified amount of bytes.
+    ReadCursor(usize),
 
+    /// Sets the peek cursor offset from its current position by the specified amount of bytes.
+    Current(isize),
+
+    /// Sets the peek cursor offset from the end of the stream by the specified amount of bytes.
+    ///
+    /// Warning: this will cause the entire stream to be loaded into memory.
+    End(isize),
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum UnreadPeekCursor {
     Fixed,
     Shift,
-    ShiftIfZero
+    ShiftIfZero,
 }
 
-
+#[derive(Debug)]
 pub struct PeekBytes<R> {
     inner: R,
 }
 
-// TODO:
-// impl<T: PeekRead> PeekRead for Take<T> { }
-// impl<T: PeekRead, U: PeekRead> PeekRead for Chain<T, U> { }
+impl<R: PeekRead> Iterator for PeekBytes<R> {
+    type Item = Result<u8>;
 
+    fn next(&mut self) -> Option<Result<u8>> {
+        let mut byte = 0;
+        loop {
+            return match self.inner.peek(std::slice::from_mut(&mut byte)) {
+                Ok(0) => None,
+                Ok(..) => Some(Ok(byte)),
+                Err(ref e) if e.kind() == ErrorKind::Interrupted => continue,
+                Err(e) => Some(Err(e)),
+            };
+        }
+    }
+}
 
-
-pub(crate) fn default_peek_exact<R: PeekRead + ?Sized>(
-    this: &mut R,
-    mut buf: &mut [u8],
-) -> Result<()> {
+fn default_peek_exact<R: PeekRead + ?Sized>(this: &mut R, mut buf: &mut [u8]) -> Result<()> {
     while !buf.is_empty() {
         match this.peek(buf) {
             Ok(0) => break,
@@ -86,7 +109,7 @@ pub(crate) fn default_peek_exact<R: PeekRead + ?Sized>(
                 let tmp = buf;
                 buf = &mut tmp[n..];
             }
-            Err(ref e) if e.kind() == ErrorKind::Interrupted => {}
+            Err(ref e) if e.kind() == ErrorKind::Interrupted => {} // Ignore interrupt.
             Err(e) => return Err(e),
         }
     }
@@ -99,3 +122,70 @@ pub(crate) fn default_peek_exact<R: PeekRead + ?Sized>(
         Ok(())
     }
 }
+
+fn default_peek_to_end<R: PeekRead + ?Sized>(this: &mut R, buf: &mut Vec<u8>) -> Result<usize> {
+    // This implementation is slower than theoretically necessary to avoid unsafe code.
+    let mut tmp = vec![0u8; 32];
+    let mut written = 0;
+    loop {
+        match this.peek(&mut tmp) {
+            Ok(0) => break,
+            Ok(n) => {
+                written += (&tmp[..n]).read_to_end(buf)?;
+
+                if n == tmp.len() {
+                    // We can probably do bigger reads, double buffer size.
+                    tmp.resize(n * 2, 0);
+                }
+            }
+            Err(ref e) if e.kind() == ErrorKind::Interrupted => {} // Ignore interrupt.
+            Err(e) => return Err(e),
+        }
+    }
+
+    Ok(written)
+}
+
+fn default_peek_to_string<R: PeekRead + ?Sized>(this: &mut R, buf: &mut String) -> Result<usize> {
+    // This implementation is slower than theoretically necessary to avoid unsafe code.
+    let mut tmp = Vec::new();
+    let bytes = this.read_to_end(&mut tmp)?;
+    let string = std::str::from_utf8(&tmp)
+        .map_err(|_| Error::new(ErrorKind::InvalidData, "stream did not contain valid UTF-8"))?;
+    *buf += string;
+    Ok(bytes)
+}
+
+impl<T: PeekRead> PeekRead for Take<T> {
+    fn peek_position(&self) -> usize {
+        self.get_ref().peek_position()
+    }
+
+    fn peek_seek(&mut self, pos: PeekSeekFrom) -> Result<usize> {
+        if let PeekSeekFrom::End(offset) = pos {
+            let limit = self.limit() as usize;
+            let eof_offset = self.get_mut().peek_seek(PeekSeekFrom::ReadCursor(limit))?;
+            let seek_pos = (eof_offset as isize + offset).max(0) as usize;
+            self.get_mut().peek_seek(PeekSeekFrom::ReadCursor(seek_pos))
+        } else {
+            self.get_mut().peek_seek(pos)
+        }
+    }
+
+    fn unread(&mut self, data: &[u8], peek_cursor_behavior: UnreadPeekCursor) {
+        self.get_mut().unread(data, peek_cursor_behavior);
+        self.set_limit(self.limit() + data.len() as u64);
+    }
+
+    fn peek(&mut self, buf: &mut [u8]) -> Result<usize> {
+        let remaining = (self.limit() as usize).saturating_sub(self.peek_position());
+        let max_peek = remaining.min(buf.len());
+        self.get_mut().peek(&mut buf[..max_peek])
+    }
+ }
+
+
+
+
+// TODO This looks tricky but maybe possible?
+// impl<T: PeekRead, U: PeekRead> PeekRead for Chain<T, U> { }
--- src/peekreader.rs
+++ src/peekreader.rs
@@ -1,22 +1,29 @@
 use std::io::{BufRead, Error, ErrorKind, Read, Result};
 
-use crate::PeekRead;
+use crate::{PeekRead, PeekSeekFrom, UnreadPeekCursor};
 
+/// A type implementing the functionality of PeekRead akin
+/// to how BufReader implements BufRead.
 pub struct PeekReader<R> {
+    // Where we store the peeked but not yet read data.
+    // This data lives in the buffer buf_storage[buf_begin..].
+    // We can thus have free space at the front.
     buf_storage: Vec<u8>,
     buf_begin: usize,
-    peek_pos: usize,
+    desired_front_space: usize,
+
+    peek_pos: usize, // This is relative to the read pointer.
     inner: R,
 }
 
 impl<R: Read> PeekReader<R> {
     const MIN_READ: usize = 8 * 1024;
-    const MIN_GC_SIZE: usize = 3 * Self::MIN_READ;
 
     pub fn new(reader: R) -> Self {
         Self {
             buf_storage: Vec::new(),
             buf_begin: 0,
+            desired_front_space: 32,
             peek_pos: 0,
             inner: reader,
         }
@@ -26,10 +33,12 @@ impl<R: Read> PeekReader<R> {
         &self.buf_storage[self.buf_begin..]
     }
 
-    fn ensure_buffer_size(&mut self, nbytes: usize) -> Result<()> {
+    // Try to fill the buffer so that it's at least nbytes in length
+    // (may fail to do so if EOF is reached - no error is reported then).
+    fn request_buffer(&mut self, nbytes: usize) -> Result<()> {
         let nbytes_needed = nbytes.saturating_sub(self.buffer().len());
         if nbytes_needed > 0 {
-            self.gc_buffer();
+            self.request_space_at_end();
             let read_size = nbytes_needed.max(Self::MIN_READ);
             self.inner
                 .by_ref()
@@ -39,28 +48,53 @@ impl<R: Read> PeekReader<R> {
         Ok(())
     }
 
-    fn gc_buffer(&mut self) {
+    // Ensure the buffer to be at least nbytes in length.
+    fn ensure_buffer(&mut self, nbytes: usize) -> Result<()> {
+        self.request_buffer(nbytes)?;
+        if self.buffer().len() < nbytes {
+            Err(Error::new(ErrorKind::UnexpectedEof, "failed to fill peek buffer"))
+        } else {
+            Ok(())
+        }
+    }
+
+    fn request_space_at_end(&mut self) {
         // If our capacity is at least half unused (and sufficiently big),
         // move the elements back to the start.
         let cap = self.buf_storage.capacity();
-        if cap >= Self::MIN_GC_SIZE && self.buf_begin >= self.buf_storage.capacity() / 2 {
-            self.buf_storage.drain(0..self.buf_begin);
+        if cap >= 3 * Self::MIN_READ && self.buf_begin >= self.buf_storage.capacity() / 2 {
+            // Shrink desired front space a bit since we're relatively lacking space at the end.
+            self.desired_front_space = self.desired_front_space.min(self.buf_begin) * 2 / 3;
+            self.buf_storage.drain(self.desired_front_space..self.buf_begin);
             self.buf_begin = 0;
+        }
+    }
+
+    fn ensure_space_at_front(&mut self, nbytes: usize) {
+        if nbytes > self.buf_begin {
+            // Not enough space at the front, we have to reallocate or move elements. To prevent
+            // this from occurring all the time when doing big unreads, increase our desired front space.
+            self.desired_front_space = 2 * self.desired_front_space.max(nbytes) + 32;
+            let extra_front_space = self.desired_front_space - self.buf_begin;
+            self.buf_storage.splice(0..0, std::iter::repeat(0).take(extra_front_space));
+            self.buf_begin += extra_front_space;
         }
     }
 }
 
 impl<R: Read> PeekRead for PeekReader<R> {
     fn peek(&mut self, buf: &mut [u8]) -> Result<usize> {
-        self.ensure_buffer_size(self.peek_pos + buf.len().min(Self::MIN_READ))?;
-        let written = (&self.buffer()[self.peek_pos..]).read(buf).unwrap(); // Can't fail.
+        self.request_buffer(self.peek_pos + buf.len().min(Self::MIN_READ))?;
+        let mut peek_buffer = self.buffer().get(self.peek_pos..).unwrap_or_default();
+        let written = peek_buffer.read(buf).unwrap(); // Can't fail.
         self.peek_pos += written;
         Ok(written)
     }
 
     fn peek_exact(&mut self, buf: &mut [u8]) -> Result<()> {
-        self.ensure_buffer_size(self.peek_pos + buf.len())?;
-        self.peek_pos += (&self.buffer()[self.peek_pos..]).read(buf).unwrap(); // Can't fail.
+        self.ensure_buffer(self.peek_pos + buf.len())?;
+        let mut peek_buffer = &self.buffer()[self.peek_pos..];
+        self.peek_pos += peek_buffer.read(buf).unwrap(); // Can't fail.
         Ok(())
     }
 
@@ -68,8 +102,27 @@ impl<R: Read> PeekRead for PeekReader<R> {
         self.peek_pos
     }
 
-    fn set_peek_position(&mut self, pos: usize) {
-        self.peek_pos = pos;
+    fn peek_seek(&mut self, pos: PeekSeekFrom) -> Result<usize> {
+        match pos {
+            PeekSeekFrom::ReadCursor(offset) => self.peek_pos = offset as usize,
+            PeekSeekFrom::Current(offset) => self.peek_pos = (self.peek_pos as isize + offset).max(0) as usize,
+            PeekSeekFrom::End(offset) => {
+                self.request_buffer(usize::MAX)?;
+                self.peek_pos = (self.buffer().len() as isize + offset).max(0) as usize;
+            }
+        }
+        Ok(self.peek_pos)
+    }
+
+    fn unread(&mut self, data: &[u8], peek_cursor_behavior: UnreadPeekCursor) {
+        let n = data.len();
+        self.ensure_space_at_front(n);
+        if peek_cursor_behavior == UnreadPeekCursor::Fixed ||
+            peek_cursor_behavior == UnreadPeekCursor::ShiftIfZero && self.peek_pos > 0 {
+            self.peek_pos += n;
+        }
+        self.buf_begin -= n;
+        (&mut self.buf_storage[self.buf_begin..self.buf_begin+n]).copy_from_slice(data)
     }
 }
 
@@ -106,3 +159,4 @@ impl<R: Read> BufRead for PeekReader<R> {
         self.peek_pos = self.peek_pos.saturating_sub(amt);
     }
 }
+
