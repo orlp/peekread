591b62bc2dae160ba770fbb0d7965ceda81fa846
Orson Peters <orsonpeters@gmail.com>
2021-06-01 02:14:24 +0200

    Removed useless BufRead requirement and added Take back.

--- src/detail/mod.rs
+++ src/detail/mod.rs
@@ -15,45 +15,45 @@ use cursor::DefaultImplPeekCursor;
 /// available to aid you in case the object you're implementing [`PeekReadImpl`] on does not
 /// have the needed storage available to keep the cursor state (e.g. the [`PeekRead`] implementation
 /// for `&[u8]`).
-pub trait PeekReadImpl: BufRead {
+pub trait PeekReadImpl {
     /// Used to implement `self.peek().seek(pos)`. See [`Seek::seek`].
-    fn peek_seek(&mut self, state: &mut PeekCursorState, pos: SeekFrom) -> Result<u64>;
+    fn peek_seek<'a>(&'a mut self, state: &'a mut PeekCursorState, pos: SeekFrom) -> Result<u64>;
     
     /// Used to implement `self.peek().read(buf)`. See [`Read::read`].
-    fn peek_read(&mut self, state: &mut PeekCursorState, buf: &mut [u8]) -> Result<usize>;
+    fn peek_read<'a, 'b>(&'a mut self, state: &'a mut PeekCursorState, buf: &'b mut [u8]) -> Result<usize>;
 
     /// Used to implement `self.peek().fill_buf()`. See [`BufRead::fill_buf`].
-    fn peek_fill_buf(&mut self, state: &mut PeekCursorState) -> Result<&[u8]>;
+    fn peek_fill_buf<'a>(&'a mut self, state: &'a mut PeekCursorState) -> Result<&'a [u8]>;
 
     /// Used to implement `self.peek().consume()`. See [`BufRead::consume`].
-    fn peek_consume(&mut self, state: &mut PeekCursorState, amt: usize);
+    fn peek_consume<'a>(&'a mut self, state: &'a mut PeekCursorState, amt: usize);
 
 
     // Start default methods.
     /// Used to implement `self.peek().stream_position()`. See [`Seek::stream_position`].
-    fn peek_stream_position(&mut self, state: &mut PeekCursorState) -> Result<u64> {
+    fn peek_stream_position<'a>(&'a mut self, state: &'a mut PeekCursorState) -> Result<u64> {
         DefaultImplPeekCursor::new(self, state).stream_position()
     }
 
     /// Used to implement `self.peek().read_exact(buf)`. See [`Read::read_exact`].
-    fn peek_read_exact(&mut self, state: &mut PeekCursorState, buf: &mut [u8]) -> Result<()> {
+    fn peek_read_exact<'a, 'b>(&'a mut self, state: &'a mut PeekCursorState, buf: &'b mut [u8]) -> Result<()> {
         DefaultImplPeekCursor::new(self, state).read_exact(buf)
     }
 
     /// Used to implement `self.peek().read_to_end(buf)`. See [`Read::read_to_end`].
-    fn peek_read_to_end(
-        &mut self,
-        state: &mut PeekCursorState,
-        buf: &mut Vec<u8>,
+    fn peek_read_to_end<'a, 'b>(
+        &'a mut self,
+        state: &'a mut PeekCursorState,
+        buf: &'b mut Vec<u8>,
     ) -> Result<usize> {
         DefaultImplPeekCursor::new(self, state).read_to_end(buf)
     }
 
     /// Used to implement `self.peek().read_to_string(buf)`. See [`Read::read_to_string`].
-    fn peek_read_to_string(
-        &mut self,
-        state: &mut PeekCursorState,
-        buf: &mut String,
+    fn peek_read_to_string<'a, 'b>(
+        &'a mut self,
+        state: &'a mut PeekCursorState,
+        buf: &'b mut String,
     ) -> Result<usize> {
         DefaultImplPeekCursor::new(self, state).read_to_string(buf)
     }
--- src/foreign_impl.rs
+++ src/foreign_impl.rs
@@ -1,6 +1,7 @@
 use crate::{PeekRead, PeekCursor};
 use crate::detail::{PeekReadImpl, PeekCursorState};
 use std::io::*;
+use std::io;
 
 fn add_offset(base: u64, offset: i64) -> u64 {
     (base as i64).saturating_add(offset).max(0) as u64
@@ -83,24 +84,14 @@ impl<T: AsRef<[u8]>> PeekRead for Cursor<T> {
 
 impl<T: AsRef<[u8]>> PeekReadImpl for Cursor<T> {
     fn peek_seek(&mut self, state: &mut PeekCursorState, pos: SeekFrom) -> Result<u64> {
-        let slice = self.get_ref().as_ref();
-        state.peek_pos = match pos {
-            SeekFrom::Start(offset) => offset,
-            SeekFrom::Current(offset) => add_offset(state.peek_pos, offset),
-            SeekFrom::End(offset) => add_offset(slice.len() as u64, offset),
-        };
-        state.peek_pos = state.peek_pos.min(slice.len() as u64);
-        Ok(state.peek_pos)
+        self.get_ref().as_ref().peek_seek(state, pos)
     }
 
     fn peek_read(&mut self, state: &mut PeekCursorState, buf: &mut [u8]) -> Result<usize> {
-        let slice = self.get_ref().as_ref();
-        let written = slice.get(state.peek_pos as usize..).unwrap_or_default().read(buf)?;
-        state.peek_pos += written as u64;
-        Ok(written)
+        self.get_ref().as_ref().peek_read(state, buf)
     }
 
-    fn peek_fill_buf(&mut self, state: &mut PeekCursorState) -> Result<&[u8]> {
+    fn peek_fill_buf<'a>(&'a mut self, state: &'a mut PeekCursorState) -> Result<&'a [u8]> {
         let slice = self.get_ref().as_ref();
         Ok(slice.get(state.peek_pos as usize..).unwrap_or_default())
     }
@@ -109,6 +100,110 @@ impl<T: AsRef<[u8]>> PeekReadImpl for Cursor<T> {
         state.peek_pos += amt as u64;
     }
 }
+
+impl<T: PeekRead> PeekRead for Take<T> {
+    fn peek(&mut self) -> PeekCursor<'_> {
+        PeekCursor::new(self)
+    }
+}
+
+impl<T: PeekRead> PeekReadImpl for Take<T> {
+    fn peek_seek(&mut self, state: &mut PeekCursorState, pos: SeekFrom) -> Result<u64> {
+        let limit_from_start = state.peek_pos + self.limit();
+        state.peek_pos = match pos {
+            SeekFrom::Start(offset) => offset,
+            SeekFrom::Current(offset) => add_offset(state.peek_pos, offset),
+            SeekFrom::End(offset) => {
+                let end = io::copy(&mut self.peek().take(limit_from_start), &mut io::sink())?;
+                add_offset(end, offset)
+            }
+        };
+        self.set_limit(limit_from_start.saturating_sub(state.peek_pos));
+        Ok(state.peek_pos)
+    }
+
+    fn peek_read(&mut self, state: &mut PeekCursorState, buf: &mut [u8]) -> Result<usize> {
+        if self.limit() == 0 {
+            return Ok(0);
+        }
+
+        let limit = self.limit();
+        let mut reader = self.peek();
+        reader.seek(SeekFrom::Start(state.peek_pos))?;
+        let written = reader.take(limit).read(buf)? as u64;
+        state.peek_pos += written;
+        self.set_limit(limit - written);
+        Ok(written as usize)
+    }
+
+    fn peek_fill_buf<'a>(&'a mut self, state: &'a mut PeekCursorState) -> Result<&'a [u8]> {
+        if self.limit() == 0 {
+            return Ok(&[]);
+        }
+
+        let limit = self.limit();
+        let mut reader = self.peek();
+        reader.seek(SeekFrom::Start(state.peek_pos))?;
+        reader.take(limit.min(state.buffer_size as u64)).read_to_end(&mut state.buf)?;
+        state.buf_pos = 0;
+        Ok(&state.buf)
+    }
+
+    fn peek_consume(&mut self, state: &mut PeekCursorState, amt: usize) {
+        let limit = self.limit();
+        state.peek_pos += amt as u64;
+        self.set_limit(limit.saturating_sub(amt as u64));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+// impl<T: PeekReadImpl> PeekReadImpl for Take<T> {
+//     fn peek_seek(&mut self, state: &mut PeekCursorState, pos: SeekFrom) -> Result<u64> {
+//         let limit = self.limit();
+//         state.peek_pos = match pos {
+//             SeekFrom::Start(offset) => offset,
+//             SeekFrom::Current(offset) => add_offset(state.peek_pos, offset),
+//             SeekFrom::End(offset) => add_offset(limit as u64, offset),
+//         };
+//     }
+
+//     fn peek_read(&mut self, state: &mut PeekCursorState, buf: &mut [u8]) -> Result<usize> {
+//         let limit = self.limit();
+//         let remaining = limit.saturating_sub(self.peek_stream_position(state)?) as usize;
+//         dbg!(remaining);
+//         let max_peek = remaining.min(buf.len());
+//         let written = self.get_mut().peek_read(state, &mut buf[..max_peek])?;
+
+//     }
+
+//     fn peek_fill_buf(&mut self, state: &mut PeekCursorState) -> Result<&[u8]> {
+//         let limit = self.limit() as usize;
+//         if limit == 0 {
+//             return Ok(&[]);
+//         }
+
+//         let buf = self.get_mut().peek_fill_buf(state)?;
+//         let n = buf.len().min(limit);
+//         Ok(&buf[..n])
+//     }
+
+//     fn peek_consume(&mut self, state: &mut PeekCursorState, amt: usize) {
+//         self.get_mut().consume(amt);
+//         let limit = self.limit();
+//         self.set_limit(limit.saturating_sub(amt as u64));
+//     }
+
+// }
+
+
 
 
 
